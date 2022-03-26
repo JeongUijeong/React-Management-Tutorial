8a14d02926c8bac9bb97210de728759a96b121ad
JeongUijeong <jeong275117@gmail.com>
2022-03-27 01:04:52 +0900

    Add Get Customers RESTAPI Module

--- client/src/App.js
+++ client/src/App.js
@@ -19,51 +19,47 @@ const styles = theme => ({
   table: {
     minWidth:1080
   }
-})
+});
 
-const customers = [{
-  'id' : 1, 
-  'image' : 'https://placeimg.com/64/64/1', 
-  'name' : '정의정', 
-  'birthday' : '991117', 
-  'gender' : '여자', 
-  'job' : '대학생'
-},
-{
-  'id' : 2, 
-  'image' : 'https://placeimg.com/64/64/2', 
-  'name' : '최영석', 
-  'birthday' : '990407', 
-  'gender' : '남자', 
-  'job' : '대학생'
-},
-{
-  'id' : 3, 
-  'image' : 'https://placeimg.com/64/64/3', 
-  'name' : '동그니', 
-  'birthday' : '990321', 
-  'gender' : '남자', 
-  'job' : '대학생'
-}]
 class App extends Component {
+
+  state = { //변경될 수 있는 변수
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err)); //오류 확인
+  }
+
+  callApi = async () => { //비동기적으로 내용 수행
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return(
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
-            { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) }) }
+            { this.state.customers ? this.state.customers.map(c => { 
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) 
+            }) : ""}
           </TableBody>
         </Table>
-        
       </Paper>
     );
   }
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+      {
+        'id' : 1, 
+        'image' : 'https://placeimg.com/64/64/1', 
+        'name' : '정의정', 
+        'birthday' : '991117', 
+        'gender' : '여자', 
+        'job' : '대학생'
+      },
+      {
+        'id' : 2, 
+        'image' : 'https://placeimg.com/64/64/2', 
+        'name' : '최영석', 
+        'birthday' : '990407', 
+        'gender' : '남자', 
+        'job' : '대학생'
+      },
+      {
+        'id' : 3, 
+        'image' : 'https://placeimg.com/64/64/3', 
+        'name' : '동그니', 
+        'birthday' : '990321', 
+        'gender' : '남자', 
+        'job' : '대학생'
+      }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
